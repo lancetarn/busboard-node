6c4d1c98baf50dec5a0c3328bebce679c9d9dac9
Lance Erickson <lancetarn@gmail.com>
2013-12-08 16:13:16 -0500

    Typo in broker require

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 /*
  * Serve JSON to our AngularJS client
  */
-var mongoBroker = require( '../mongoBroker' );
+var mongoBroker = require( '../mongobroker' );
 var UserModel = require( '../userModel' );
 var Users = require( '../users' );
 exports.users = new Users( new UserModel( mongoBroker ) );
--- test/busboard/userModel-spec.js
+++ test/busboard/userModel-spec.js
@@ -1,7 +1,7 @@
 
 describe( 'userModel', function( ) {
 
-    var broker = require( '../../mongoBroker' );
+    var broker = require( '../../mongobroker' );
     var user = { 'user' : 'testuser', 'pass' : 'testpass' };
 
     it( 'Should check if username is in use before adding', function( ) {} );
